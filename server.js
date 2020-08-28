68ef59f9547ff41eff37d7b8b017969253439e9f
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-08-28 23:39:34 +0000

    add graphQl server

---
+++ server.js
@@ -0,0 +1,8 @@
+ 
+const { GraphQLServer } = require("graphql-yoga");
+
+const server = new GraphQLServer();
+
+server.start(({port}) => {
+    console.log(`Server on http://localhost:${port}`);
+})
