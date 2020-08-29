8829a1c22527d0ffb103793abe0df3fa1cbdb51a
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-08-29 23:36:00 +0000

    add shards-react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Alert } from "shards-react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css"
 
 import "./index.css";
 
