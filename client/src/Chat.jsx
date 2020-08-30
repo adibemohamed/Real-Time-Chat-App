36511dbc4f3dc440e04e6325fb8f98ebe7ed8f42
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-08-30 01:21:55 +0000

    style message component

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from '@apollo/client';
+import { Container, Row, Col, FormInput, Button } from "shards-react";
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/',
@@ -16,14 +17,54 @@ const GET_MESSAGES = gql`
   }
 `;
 
-const Message = ({user}) => {
-    const {data} = useQuery(GET_MESSAGES);
-    if(!data) {
-        return null;
+const Messages = ({ user }) => {
+    const { data } = useSubscription(GET_MESSAGES);
+    if (!data) {
+      return null;
     }
-    return JSON.stringify(data);
-}
-
+  
+    return (
+      <>
+        {data.messages.map(({ id, user: messageUser, content }) => (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: user === messageUser ? "flex-end" : "flex-start",
+              paddingBottom: "1em",
+            }}
+          >
+            {user !== messageUser && (
+              <div
+                style={{
+                  height: 50,
+                  width: 50,
+                  marginRight: "0.5em",
+                  border: "2px solid #e5e6ea",
+                  borderRadius: 25,
+                  textAlign: "center",
+                  fontSize: "18pt",
+                  paddingTop: 5,
+                }}
+              >
+                {messageUser.slice(0, 2).toUpperCase()}
+              </div>
+            )}
+            <div
+              style={{
+                background: user === messageUser ? "blue" : "#e5e6ea",
+                color: user === messageUser ? "white" : "black",
+                padding: "1em",
+                borderRadius: "1em",
+                maxWidth: "60%",
+              }}
+            >
+              {content}
+            </div>
+          </div>
+        ))}
+      </>
+    );
+  }
 const Chat = () => {
     return <div><Message user="Adibe"/></div>
 };
